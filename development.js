64bf7b5aaa9225b0ec7c36a8e22571a7923197ed
Socratic-tech <john.phillips@berrienresa.org>
2025-02-06 10:34:22 -0500

    Update development.js

--- development.js
+++ development.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+  port: process.env.PORT || 3000,
+  puppeteerOptions: {
+    headless: true,
+    args: ['--no-sandbox']
+  },
+  maxConcurrentChecks: 5,
+  timeoutMs: 30000
+};
